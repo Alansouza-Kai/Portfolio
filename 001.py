b65b455df9f1e1353ccd67a2f427f52e17391104
Alan Souza <alllam_sousa@hotmail.com>
2025-05-18 02:01:36 -0300

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,10 @@
+'''Escreva um programa que leia, o nome, idade, e cidade de nascimento e retorne para o usuário
+Saída esperada:
+Meu nome é Luis Tatin, tenho 22 anos e nasci em São Bernardo do campo'''
+
+
+Nome = input("Escreva seu nome: ")
+Idade = int(input("Escreva sua idade: "))
+Cidade = input("Escreva sua cidade de nascimento: ")
+
+print(f'Meu nome é {Nome}, tenho {Idade} anos e nasci em {Cidade}')
